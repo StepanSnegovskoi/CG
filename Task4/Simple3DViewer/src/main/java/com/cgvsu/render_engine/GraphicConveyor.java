84c06685a6fd8c0b01b1e053c48bcb1c5acaa7c6
stepann <stepan.snegovskoi@mail.ru>
2025-01-20 16:01:05 +0300

    Update lookAt and fix bug

--- Task4/Simple3DViewer/src/main/java/com/cgvsu/render_engine/GraphicConveyor.java
+++ Task4/Simple3DViewer/src/main/java/com/cgvsu/render_engine/GraphicConveyor.java
@@ -26,13 +26,20 @@ public class GraphicConveyor {
         resultX = resultX.normalize();
         resultY = resultY.normalize();
         resultZ = resultZ.normalize();
+        Matrix4d displacementMatrix = new Matrix4d(new float[][] {
+                {1, 0, 0, -eye.getX()},
+                {0, 1, 0, -eye.getY()},
+                {0, 0, 1, -eye.getZ()},
+                {0, 0, 0, 1}
+        });
 
-        float[][] matrix = new float[][]{
-                {resultX.getX(), resultY.getX(), resultZ.getX(), 0},
-                {resultX.getY(), resultY.getY(), resultZ.getY(), 0},
-                {resultX.getZ(), resultY.getZ(), resultZ.getZ(), 0},
-                {-resultX.dot(eye), -resultY.dot(eye), -resultZ.dot(eye), 1}};
-        return new Matrix4d(matrix);
+        Matrix4d projectionMatrix = new Matrix4d(new float[][] {
+                {resultX.getX(), resultX.getY(), resultX.getZ(), 0},
+                {resultY.getX(), resultY.getY(), resultY.getZ(), 0},
+                {resultZ.getX(), resultZ.getY(), resultZ.getZ(), 0},
+                {0, 0, 0, 1}
+        });
+        return projectionMatrix.multiply(displacementMatrix);
     }
 
     public static Matrix4d perspective(
@@ -45,17 +52,17 @@ public class GraphicConveyor {
         result[0][0] = tangentMinusOnDegree / aspectRatio;
         result[1][1] = tangentMinusOnDegree;
         result[2][2] = (farPlane + nearPlane) / (farPlane - nearPlane);
-        result[2][3] = 1.0F;
-        result[3][2] = 2 * (nearPlane * farPlane) / (nearPlane - farPlane);
+        result[3][2] = 1.0F;
+        result[2][3] = 2 * (nearPlane * farPlane) / (nearPlane - farPlane);
         return new Matrix4d(result);
     }
 
     public static Vector3f multiplyMatrix4ByVector3(final Matrix4d matrix, final Vector3f vertex) {
         float[][] matrix4d = matrix.getElements();
-        final float x = (vertex.getX() * matrix4d[0][0]) + (vertex.getY() * matrix4d[1][0]) + (vertex.getZ() * matrix4d[2][0]) + matrix4d[3][0];
-        final float y = (vertex.getX() * matrix4d[0][1]) + (vertex.getY() * matrix4d[1][1]) + (vertex.getZ() * matrix4d[2][1]) + matrix4d[3][1];
-        final float z = (vertex.getX() * matrix4d[0][2]) + (vertex.getY() * matrix4d[1][2]) + (vertex.getZ() * matrix4d[2][2]) + matrix4d[3][2];
-        final float w = (vertex.getX() * matrix4d[0][3]) + (vertex.getY() * matrix4d[1][3]) + (vertex.getZ() * matrix4d[2][3]) + matrix4d[3][3];
+        final float x = (vertex.getX() * matrix4d[0][0]) + (vertex.getY() * matrix4d[0][1]) + (vertex.getZ() * matrix4d[0][2]) + matrix4d[0][3];
+        final float y = (vertex.getX() * matrix4d[1][0]) + (vertex.getY() * matrix4d[1][1]) + (vertex.getZ() * matrix4d[1][2]) + matrix4d[1][3];
+        final float z = (vertex.getX() * matrix4d[2][0]) + (vertex.getY() * matrix4d[2][1]) + (vertex.getZ() * matrix4d[2][2]) + matrix4d[2][3];
+        final float w = (vertex.getX() * matrix4d[3][0]) + (vertex.getY() * matrix4d[3][1]) + (vertex.getZ() * matrix4d[3][2]) + matrix4d[3][3];
         return new Vector3f(x / w, y / w, z / w);
     }
 
